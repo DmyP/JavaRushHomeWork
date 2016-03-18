ea486bd757c88e29a84544c73dd473545f00be38
DmyP <pechenyi.d@gmail.com>
2016-03-18 16:47:00 +0200

    Rush

---
+++ src/test/test.java
@@ -0,0 +1,13 @@
+package test;
+
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+       String i = sc.nextLine();
+        System.out.println(i);
+    }
+
+
+}
