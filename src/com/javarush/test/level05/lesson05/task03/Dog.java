aa74cfaef7fd7503dfa465c39bc06381186fbc47
dmyp <pechenyi.d@gmail.com>
2016-03-27 11:44:12 +0300

    rush5

--- src/com/javarush/test/level05/lesson05/task03/Dog.java
+++ src/com/javarush/test/level05/lesson05/task03/Dog.java
@@ -8,5 +8,21 @@ package com.javarush.test.level05.lesson05.task03;
 public class Dog {
     private String name;
     private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
     //добавьте тут геттеры и сеттеры
 }
