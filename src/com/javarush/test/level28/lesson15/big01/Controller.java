ac10f898adcfadb81201e7f2f06104d529d23b7f
DmyP <pechenyi.d@gmail.com>
2016-04-28 15:56:06 +0300

    rush28

--- src/com/javarush/test/level28/lesson15/big01/Controller.java
+++ src/com/javarush/test/level28/lesson15/big01/Controller.java
@@ -1,38 +1,15 @@
 package com.javarush.test.level28.lesson15.big01;
 
-import com.javarush.test.level28.lesson15.big01.model.Provider;
-import com.javarush.test.level28.lesson15.big01.vo.Vacancy;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import com.javarush.test.level28.lesson15.big01.model.Model;
 
 public class Controller {
-    private Provider[] providers;
+    private Model model;
 
-    public Controller(Provider... providers) {
-        if (providers.length == 0) throw new IllegalArgumentException();
-        this.providers = providers;
-
+    public Controller(Model model) {
+        if (model == null) throw new IllegalArgumentException();
+        this.model = model;
     }
-
-    @Override
-    public String toString() {
-        return "Controller{providers=" + Arrays.toString(providers) + '}';
+    public void onCitySelect(String cityName) {
+        model.selectCity(cityName);
     }
-
-
-    public void scan() {
-        List<Vacancy> vacancies = new ArrayList<>();
-        for (Provider provider : providers) {
-            try {
-                for (Vacancy vac : provider.getJavaVacancies("Kiev")) {
-                    vacancies.add(vac);
-                }
-            }
-            catch (NullPointerException e) {}
-        }
-        System.out.println(vacancies.size());
-    }
-
 }
--- src/com/javarush/test/level28/lesson15/big01/model/HHStrategy.java
+++ src/com/javarush/test/level28/lesson15/big01/model/HHStrategy.java
@@ -3,23 +3,54 @@ package com.javarush.test.level28.lesson15.big01.model;
 import com.javarush.test.level28.lesson15.big01.vo.Vacancy;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.List;
 
 public class HHStrategy implements Strategy {
     private static final String URL_FORMAT = "http://hh.ua/search/vacancy?text=java+%s&page=%d";
-
+    private static final String USERAGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36";
+    private static final String REFERRER = "http://www.google.com.ua";
+    private static final int TIMEOUT = 5 * 1000;
     @Override
     public List<Vacancy> getVacancies(String searchString) {
+        List<Vacancy> list = new ArrayList<>();
+
+        int i = 0;
+        while (true) {
+            try {
+                Document document = getDocument(searchString, i++);
+                Elements elements = document.getElementsByAttributeValue("data-qa","vacancy-serp__vacancy");
+                if (elements.size() > 0) {
+                    for (Element element : elements) {
+                        Vacancy vvacancyc = new Vacancy();
+                        vvacancyc.setUrl(element.getElementsByAttributeValue("data-qa", "vacancy-serp__vacancy" + "-title").attr("href"));
+                        vvacancyc.setTitle(element.getElementsByAttributeValue("data-qa", "vacancy-serp__vacancy" + "-title").text());
+                        vvacancyc.setCity(element.getElementsByAttributeValue("data-qa", "vacancy-serp__vacancy" + "-address").text());
+                        vvacancyc.setCompanyName(element.getElementsByAttributeValue("data-qa", "vacancy-serp__vacancy" + "-employer").text());
+                        Elements sal = element.getElementsByAttributeValue("data-qa", "vacancy-serp__vacancy" + "-compensation");
+                        if (sal.size() == 0) vvacancyc.setSalary("");
+                        else vvacancyc.setSalary(sal.text());
+                        vvacancyc.setSiteName(document.title());
+                        list.add(vvacancyc);
+                    }
+                }
+            } catch (IOException e) {}
+            return list;
+        }
+    }
+
+    protected  Document getDocument(String searchString, int page) throws IOException{
+        String url = String.format(URL_FORMAT, searchString, page);
         try {
-            Document document = Jsoup.connect(URL_FORMAT).userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36").referrer("http://google.ru").get();
-            int i = 1;
-            document.charset(Charset.defaultCharset());
+            Document doc = Jsoup.connect(url).userAgent(USERAGENT).referrer(REFERRER).timeout(TIMEOUT).get();
+            return doc;
         }
         catch (IOException e) {
+            return null;
         }
-        return null;
     }
 }
