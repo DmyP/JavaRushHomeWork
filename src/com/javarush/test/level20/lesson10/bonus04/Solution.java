ce0d408e42df98455b497f4282174dc3f61bbcf9
dmyp <pechenyi.d@gmail.com>
2016-07-19 19:27:09 +0300

    rush39

--- src/com/javarush/test/level20/lesson10/bonus04/Solution.java
+++ src/com/javarush/test/level20/lesson10/bonus04/Solution.java
@@ -1,8 +1,7 @@
 package com.javarush.test.level20.lesson10.bonus04;
 
-import java.io.Serializable;
-import java.util.AbstractList;
-import java.util.List;
+import java.io.*;
+import java.util.*;
 
 /* Свой список
 Посмотреть, как реализован LinkedList.
@@ -41,7 +40,6 @@ import java.util.List;
     ->8->17
  ->4->10->19
         ->21
-
 Во внутренней реализации элементы должны добавляться по 2 на каждый уровень
 Метод getParent должен возвращать элемент, который на него ссылается.
 Например, 3 ссылается на 7 и на 8, т.е.  getParent("8")=="3", а getParent("13")=="6"
@@ -53,38 +51,155 @@ import java.util.List;
 Метод main в тестировании не участвует
 */
 public class Solution extends AbstractList<String> implements List<String>, Cloneable, Serializable {
-    public static void main(String[] args) {
-//        List<String> list = new Solution();
-//        for (int i = 1; i < 16; i++) {
-//            list.add(String.valueOf(i));
-//        }
-//        System.out.println("Expected 3, actual is " + ((Solution) list).getParent("8"));
-//        list.remove("5");
-//        System.out.println("Expected null, actual is " + ((Solution) list).getParent("11"));
+    private TreeNode root = new TreeNode();
+
+    class TreeNode implements Serializable {
+        private String data;
+        private TreeNode parent;
+        private TreeNode left;
+        private TreeNode right;
+
+        public TreeNode() {}
+
+        public TreeNode(TreeNode parent, String value) {
+            this.data = value;
+            this.parent = parent;
+        }
     }
 
     public String getParent(String value) {
-        //have to be implemented
+        ArrayDeque<TreeNode> arrayDeque = new ArrayDeque<>();
+        arrayDeque.add(root);
+        while (!arrayDeque.isEmpty()) {
+            TreeNode node = arrayDeque.poll();
+
+            if (value.equals(node.data)) {
+                return node.parent.data;
+            }
+            else {
+                if (node.left != null) arrayDeque.add(node.left);
+                if (node.right != null) arrayDeque.add(node.right);
+            }
+        }
         return null;
     }
 
     @Override
-    public boolean add(String s) {
-        return super.add(s);
+    public Iterator<String> iterator() {
+        return new NodeIterator();
+    }
+
+    private class NodeIterator implements Iterator<String> {
+        private ArrayList<String> nodes = new ArrayList<>();
+        private Iterator<String> iterator;
+        private String currentValue;
+
+        public NodeIterator() {
+            ArrayDeque<TreeNode> arrayDeque = new ArrayDeque<>();
+            arrayDeque.add(root);
+
+            while (!arrayDeque.isEmpty()) {
+                TreeNode node = arrayDeque.poll();
+
+                if (node.data != null) nodes.add(node.data);
+
+                if (node.left != null) arrayDeque.add(node.left);
+                if (node.right != null) arrayDeque.add(node.right);
+            }
+
+            iterator = nodes.iterator();
+        }
+
+        @Override
+        public boolean hasNext() {
+            return iterator.hasNext();
+        }
+
+        @Override
+        public String next() {
+            currentValue = iterator.next();
+            return currentValue;
+        }
+
+        @Override
+        public void remove() {
+            Solution.this.remove(currentValue);
+        }
     }
 
     @Override
-    public String remove(int index) {
-        return super.remove(index);
+    public void clear() {
+        root = new TreeNode();
     }
 
     @Override
-    public String get(int index) {
-        return null;
+    public boolean remove(Object obj) {
+        ArrayDeque<TreeNode> arrayDeque = new ArrayDeque<>();
+        arrayDeque.add(root);
+        String data = (String)obj;
+        while (!arrayDeque.isEmpty()) {
+            TreeNode node = arrayDeque.poll();
+            if (data.equals(node.data)) {
+                TreeNode parent = node.parent;
+                if (parent.left == node) parent.left = null;
+                else parent.right = null;
+                return true;
+            }
+            else {
+                if (node.left != null) arrayDeque.add(node.left);
+                if (node.right != null) arrayDeque.add(node.right);
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean add(String str) {
+        ArrayDeque<TreeNode> arrayDeque = new ArrayDeque<>();
+        arrayDeque.add(root);
+        while (true) {
+            TreeNode node = arrayDeque.poll();
+            if (node.left == null) {
+                node.left = new TreeNode(node, str);
+                break;
+            }
+            else arrayDeque.add(node.left);
+
+            if (node.right == null) {
+                node.right = new TreeNode(node, str);
+                break;
+            }
+            else arrayDeque.add(node.right);
+        }
+        return true;
     }
 
     @Override
     public int size() {
-        return 0;
+        ArrayDeque<TreeNode> arrayDeque = new ArrayDeque<>();
+        arrayDeque.add(root);
+        int size = 0;
+        while (!arrayDeque.isEmpty()) {
+            TreeNode node = arrayDeque.poll();
+            size++;
+            if (node.left != null) arrayDeque.add(node.left);
+            if (node.right != null) arrayDeque.add(node.right);
+        }
+        return size - 1;
+    }
+
+    @Override
+    public String get(int index) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        List<String> list = new Solution();
+        for (int i = 1; i < 16; i++) {
+            list.add(String.valueOf(i));
+        }
+        System.out.println("Expected 3, actual is " + ((Solution) list).getParent("8"));
+        list.remove("5");
+        System.out.println("Expected null, actual is " + ((Solution) list).getParent("11"));
     }
 }
